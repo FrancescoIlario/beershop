eff4369ddf3520687c39d5d224314083291a19ce
Francesco Ilario <fr.ilario@gmail.com>
2020-10-04 01:42:46 +0200

    Server main

---
+++ cmd/server/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/FrancescoIlario/beershop/pkg/http/rest"
+	"github.com/FrancescoIlario/beershop/pkg/storage/inmem"
+)
+
+const addr = ":8080"
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "error executing server: %v", err)
+	}
+}
+
+func run() error {
+	fmt.Println("running server")
+	db := inmem.New()
+	server := rest.NewServer(db)
+
+	fmt.Printf("listening on %s", addr)
+	err := http.ListenAndServe(addr, server)
+	return err
+}
