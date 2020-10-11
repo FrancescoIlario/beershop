5c499c8f512fee92827f6ce4f05b636390815e3b
Francesco Ilario <fr.ilario@gmail.com>
2020-10-12 01:20:26 +0200

    Reading from env vars

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/FrancescoIlario/beershop/internal/http/rest"
@@ -12,15 +13,22 @@ import (
 	_ "github.com/lib/pq"
 )
 
-// TODO: read them from ENV
 const (
-	addr       = ":8080"
-	dbUser     = "postgres"
-	dbPassword = "supersecret"
-	dbHost     = "postgres"
-	dbPort     = "5432"
-	dbRetries  = 5
-	dbTimeout  = 2000
+	EnvAddr       = "ADDR"
+	EnvDbUser     = "DB_USER"
+	EnvDbPassword = "DB_PASSWORD"
+	EnvDbHost     = "DB_HOST"
+	EnvDbPort     = "DB_PORT"
+	EnvDbRetries  = "DB_RETRIES"
+	EnvDbTimeout  = "DB_TIMEOUT"
+
+	DefaultAddr       = ":8080"
+	DefaultDbUser     = "postgres"
+	DefaultDbPassword = "supersecret"
+	DefaultDbHost     = "localhost"
+	DefaultDbPort     = "5432"
+	DefaultDbRetries  = 5
+	DefaultDbTimeout  = 2000
 )
 
 func main() {
@@ -31,22 +39,68 @@ func main() {
 
 func run() error {
 	fmt.Println("connecting to database")
-	db, err := sql.Open("postgres", fmt.Sprintf("postgres://%s:%s@%s:%s/?sslmode=disable", dbUser, dbPassword, dbHost, dbPort))
+	dbr, err := dbRetries()
 	if err != nil {
 		return err
 	}
-	for i := 1; i <= dbRetries; i++ {
+	dbt, err := dbTimeout()
+	if err != nil {
+		return err
+	}
+
+	addr := address()
+	db, err := sql.Open("postgres", fmt.Sprintf("postgres://%s:%s@%s:%s/?sslmode=disable", dbUser(), dbPassword(), dbHost(), dbPort()))
+	if err != nil {
+		return err
+	}
+
+	for i := 1; i <= dbr; i++ {
 		if err := db.Ping(); err != nil {
-			if i == dbRetries {
+			if i == dbr {
 				return err
 			}
-			fmt.Printf("can not connect to db, retrying in %v (attempt %v/%v): %v\n", dbTimeout, i, dbRetries, err)
-			time.Sleep(dbTimeout * time.Millisecond)
+			fmt.Printf("can not connect to db, retrying in %v (attempt %v/%v): %v\n", dbt, i, dbr, err)
+			time.Sleep(time.Duration(dbt) * time.Millisecond)
 		}
 	}
 	repo := bsql.New(db)
 
-	fmt.Printf("listening on %s", addr)
 	server := rest.NewServer(repo)
+	fmt.Printf("listening on %s\n", addr)
 	return http.ListenAndServe(addr, server)
+}
+
+func address() string {
+	return fromEnvOrDefault(EnvAddr, DefaultAddr)
+}
+func dbUser() string {
+	return fromEnvOrDefault(EnvDbUser, DefaultDbUser)
+}
+func dbPassword() string {
+	return fromEnvOrDefault(EnvDbPassword, DefaultDbPassword)
+}
+func dbHost() string {
+	return fromEnvOrDefault(EnvDbHost, DefaultDbHost)
+}
+func dbPort() string {
+	return fromEnvOrDefault(EnvDbPort, DefaultDbPort)
+}
+func dbRetries() (int, error) {
+	return fromEnvOrDefaultInt(EnvDbRetries, DefaultDbRetries)
+}
+func dbTimeout() (int, error) {
+	return fromEnvOrDefaultInt(EnvDbTimeout, DefaultDbTimeout)
+}
+
+func fromEnvOrDefault(env, def string) string {
+	if p := os.Getenv(env); p != "" {
+		return p
+	}
+	return def
+}
+func fromEnvOrDefaultInt(env string, def int) (int, error) {
+	if p := os.Getenv(env); p != "" {
+		return strconv.Atoi(p)
+	}
+	return def, nil
 }
