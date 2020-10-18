cca283cf25612cf9c54482d1b5c7a483e6e1b45b
Francesco Ilario <fr.ilario@gmail.com>
2020-10-19 00:02:24 +0200

    Mocks

--- mocks.go
+++ mocks.go
@@ -1,3 +1,5 @@
 package beershop
 
 //go:generate mockgen -source storage.go -package=mocks -destination=internal/mocks/storage.go github.com/FrancescoIlario/beershop/storage.go Repository
+//go:generate mockgen -source backend.go -package=mocks -destination=internal/mocks/backend.go github.com/FrancescoIlario/beershop/backend.go Backend
+//go:generate mockgen -source validation.go -package=mocks -destination=internal/mocks/validation.go github.com/FrancescoIlario/beershop/validation.go ValidationResult
